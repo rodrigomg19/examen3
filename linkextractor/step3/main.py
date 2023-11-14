add526dbc41ef2d21c0b54919e2a7fb35ee5b1ec
rodrigomg19 <67974763+rodrigomg19@users.noreply.github.com>
2023-11-14 14:34:48 -0500

    Add files via upload

---
+++ linkextractor/step3/main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+# Crear un DataFrame con una sola columna que contenga el mensaje
+data = {'Mensaje': ['Hello, World!']}
+df = pd.DataFrame(data)
+
+# Imprimir el DataFrame
+print(df)
